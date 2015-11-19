2790acdb32d11388dd6ad8f2fd66a40fd4f80e18
Colin Walker <colinwalker270@gmail.com>
2015-11-19 13:11:36 -0800

    add basic projects view

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Laravel Basic Project Management</title>
+</head>
+
+<body>
+<div class="container">
+    <nav class="navbar navbar-default">
+
+    </nav>
+</div>
+
+    @yield('content')
+
+</body>
+</html>
